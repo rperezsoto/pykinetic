5d428f3a709b5f823a0294c1c48d252849499e21
Raúl Pérez-Soto <rperezsoto.research@gmail.com>
2021-03-08 12:52:14 +0100

    cleaned the templates folder of legacy files and updated the setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'pykinetic2',
   packages = ['pykinetic2'],
-  package_data = {'templates': ['templates/*.txt']},
+  package_data = {'templates': ['templates/*']},
   install_requires=["numpy", ],
   python_requires='>=3.7',
   include_package_data=True,
